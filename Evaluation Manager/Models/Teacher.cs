ffe10a425d2c444a189903873281b8aa31110e8a
kmilos21 <kmilos21@foi.hr>
2023-05-03 17:29:45 +0200

    Referencirana biblioteka za rad s bazom

--- Evaluation Manager/Models/Teacher.cs
+++ Evaluation Manager/Models/Teacher.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 
-public class Class2
-{
-	public class Teacher
+namespace Evaluation_Manager.Models {
+	public class Teacher : Person
 	{
         public string Username { get; set; }
         public string Password { get; set; }
