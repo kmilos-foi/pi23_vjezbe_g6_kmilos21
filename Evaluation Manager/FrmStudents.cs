2325ba628e1a02cc435888727eb702c29dfd2aee
kmilos21 <kmilos21@foi.hr>
2023-05-03 17:53:36 +0200

    Zadnje

--- Evaluation Manager/FrmStudents.cs
+++ Evaluation Manager/FrmStudents.cs
@@ -17,4 +17,19 @@ namespace Evaluation_Manager
             InitializeComponent();
         }
     }
+
+    private void FrmStudents_Load(object sender, EventArgs e) {
+        ShowStudents();
+    }
+
+    private void ShowStudents() {
+        List<Student> students = StudentRepository.GetStudents();
+        dgvStudent.DataSource = students;
+
+        dgvStudents.Columns["Id"].DisplayIndex = 0;
+        dgvStudents.Columns["FirstName"].DisplayIndex = 1;
+        dgvStudents.Columns["LastName"].DisplayIndex = 2;
+        dgvStudents.Columns["Grade"].DisplayIndex = 3;
+        dgvStudents.Columns["Id"].DisplayIndex = 4;
+    }
 }
---
+++ Evaluation Manager/Repositories/StudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Evaluation_Manager.Models;
+using DBLayer;
+
+namespace Evaluation_Manager.Repositories {
+	public class StudentRepository()
+	{
+		public static List<Student> GetStudents() {
+
+
+
+	     }
+	}
+}
