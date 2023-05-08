aabf843e7b9924593ae83b3f4e88961be6062453
kmilos-foi <126513322+kmilos-foi@users.noreply.github.com>
2023-05-08 16:37:19 +0200

    Delete Evaluation Manager directory

--- Evaluation Manager/Models/Teacher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Evaluation_Manager.Models {
-	public class Teacher : Person
-	{
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
